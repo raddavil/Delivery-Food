f70dcd6c668e762cbf887242ae50c7090f319654
Anton <raddavil@gmail.com>
2020-05-05 22:58:32 +0300

    js day1

--- js/main.js
+++ js/main.js
@@ -2,12 +2,87 @@ const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 
-cartButton.addEventListener('click', function (event) {
-modal.classList.add("is-open");
-});
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
 
-close.addEventListener('click', function (event) {
-modal.classList.remove("is-open");
-});
+function toggleModal() {
+	modal.classList.toggle("is-open");
+}
 
-new WOW().init();
+// day 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('gloDelivery');
+
+function toogleModalAuth() {
+	loginInput.style.borderColor = '';
+	modalAuth.classList.toggle('is-open');
+}
+
+function authorized() {
+	function logOut() {
+		login = null;
+		localStorage.removeItem('gloDelivery');
+
+		buttonAuth.style.display = '';
+		userName.style.display = '';
+		buttonOut.style.display = '';
+		buttonOut.removeEventListener('click', logOut)
+		checkAuth();
+
+	}
+
+	console.log('Авторизован');
+
+	userName.textContent = login;
+
+
+	buttonAuth.style.display = 'none';
+	userName.style.display = 'inline';
+	buttonOut.style.display = 'block';
+
+	buttonOut.addEventListener('click', logOut)
+
+}
+
+function notAuthorized() {
+	console.log('Не авторизован');
+
+	function logIn(event) {
+		event.preventDefault();
+
+if (loginInput.value.trim()){
+		login = loginInput.value;
+		localStorage.setItem('gloDelivery', login);
+		toogleModalAuth();
+		buttonAuth.removeEventListener('click', toogleModalAuth);
+		closeAuth.removeEventListener('click', toogleModalAuth);
+		logInForm.removeEventListener('submit', logIn);
+		logInForm.reset();
+		checkAuth();
+	} else {
+		loginInput.style.borderColor = 'red';
+	}
+	}
+
+
+
+	buttonAuth.addEventListener('click', toogleModalAuth);
+	closeAuth.addEventListener('click', toogleModalAuth);
+	logInForm.addEventListener('submit', logIn);
+}
+function checkAuth() {
+	if (login) {
+		authorized();
+	} else {
+		notAuthorized();
+	}
+}
+checkAuth();
